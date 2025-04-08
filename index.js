365646ca540625ab7306fa5a628e1016bfd8b9b8
Evonne1122 <ccf201610@gmail.com>
2025-04-08 18:12:02 +0800

    w8

---
+++ index.js
@@ -0,0 +1,19 @@
+function openiframe(index) {
+  const c = document.getElementsByClassName('center')[0];
+  console.log(c);
+  switch (index) {
+    case 1:
+      c.innerHTML = '<iframe src = "0225.html" frameborder="1"></iframe>';
+      break;
+    case 2:
+      c.innerHTML = '<iframe src = "W03/0304a.html" frameborder="1"></iframe>';
+      break;
+    case 3:
+      c.innerHTML = '<iframe src = "W04/0311.html" frameborder="1"></iframe>';
+      break;
+    case 4:
+      c.innerHTML = '<iframe src = "W05/0318.html" frameborder="1"></iframe>';
+      break;
+  }
+  console.log(index);
+}
