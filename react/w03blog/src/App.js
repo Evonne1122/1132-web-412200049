07677625de2a6620f42d2a8f79a7d09ee925084b
Evonne1122 <ccf201610@gmail.com>
2025-05-06 17:57:10 +0800

    mes456

---
+++ react/w03blog/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import Booklist from './singlebook';
+import { booka, bookb, bookc } from './singlebook'; /*
+const booka = {
+  img: 'https://ibw.bwnet.com.tw/image/pool/2014/12/24e79693f2d5c5902acc60dc3d23da4a.png',
+  title: 'Diary of a Wimpy Kid',
+  author: ' Jeff Kinney ',
+};*/ /*
+const bookb = {
+  img: 'https://tw-stage.blog.voicetube.com/wp-content/uploads/2014/12/0619yejiyun0.jpg',
+  title: 'The Old Man and the Sea',
+  author: 'Ernest Hemmingway ',
+};*/
+/*const bookc = {
+  img: 'https://cdn2.momjunction.com/wp-content/uploads/2015/12/The-Golden-Compass.jpg.avif',
+  title: 'The Golden Compass',
+  author: 'Philip Pullman',
+};*/
+/*
+function App() {
+  return (
+    <div className='blogs'>
+      <div className='section-title'>
+        <img src={logo} className='App-logo' alt='logo' />
+      </div>
+      <div className='blogs-center'>
+        <Blog
+          img={
+            'https://images.unsplash.com/photo-1535905557558-afc4877a26fc?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJvb2t8ZW58MHx8MHx8fDA%3D'
+          }
+          title={'Operating system concepts : Windows XP update'}
+          author={'Silberschatz, Abraham'}
+        />
+        <Blog img={booka.img} title={booka.title} author={booka.author}></Blog>
+        <Blog img={bookb.img} title={bookb.title} author={bookb.author}></Blog>
+      </div>
+    </div>
+  );
+}*/
+function App() {
+  return (
+    <div className='blogs'>
+      <div className='section-title'>
+        <h2>latest blogs</h2>
+      </div>
+      <div className='blogs-center'></div>
+      <div className='section-title'>
+        <h2>part2</h2>
+      </div>
+      <Booklist></Booklist>
+    </div>
+  );
+}
+
+const Blog = ({ img, title, author }) => {
+  return (
+    <article className='blogs'>
+      <img src={img} alt='Coffee photo' className='img blog-img' />
+      <div className='blog-content'>
+        <h3>{title}</h3>
+        <p>{author}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Booklist;
---
+++ react/w03blog/src/singlebook.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const booka = {
+  img: 'https://ibw.bwnet.com.tw/image/pool/2014/12/24e79693f2d5c5902acc60dc3d23da4a.png',
+  title: 'Diary of a Wimpy Kid',
+  author: ' Jeff Kinney ',
+};
+const bookb = {
+  img: 'https://tw-stage.blog.voicetube.com/wp-content/uploads/2014/12/0619yejiyun0.jpg',
+  title: 'The Old Man and the Sea',
+  author: 'Ernest Hemmingway ',
+};
+export const bookc = {
+  img: 'https://cdn2.momjunction.com/wp-content/uploads/2015/12/The-Golden-Compass.jpg.avif',
+  title: 'The Golden Compass',
+  author: 'Philip Pullman',
+};
+export { booka, bookb };
+
+function Blog({ img, title, author }) {
+  return (
+    <div className='blog'>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </div>
+  );
+}
+
+var data = [
+  {
+    id: 1,
+    img: 'https://ibw.bwnet.com.tw/image/pool/2014/12/24e79693f2d5c5902acc60dc3d23da4a.png',
+    title: 'Diary of a Wimpy Kid',
+    author: ' Jeff Kinney ',
+  },
+  {
+    id: 2,
+    img: 'https://tw-stage.blog.voicetube.com/wp-content/uploads/2014/12/0619yejiyun0.jpg',
+    title: 'The Old Man and the Sea',
+    author: 'Ernest Hemmingway ',
+  },
+  {
+    id: 3,
+    img: 'https://cdn2.momjunction.com/wp-content/uploads/2015/12/The-Golden-Compass.jpg.avif',
+    title: 'The Golden Compass',
+    author: 'Philip Pullman',
+  },
+];
+
+function Booklist() {
+  return (
+    <section className='blogs-center'>
+      {data.map(function callfun(value, index) {
+        const { id, img, title, author } = value;
+        return <Blog key={id} img={img} title={title} author={author}></Blog>;
+      })}
+    </section>
+  );
+}
+export default Booklist;
